a487548b35132a8864d69ca7c4803f3fbe1326ae
FrancescoPalazzo97 <francescopalazzo09@gmail.com>
2026-05-07 15:32:26 +0200

    Add IngredienteDecorator abstract class to extend Bevanda functionality

---
+++ src/models/IngredienteDecorator.java
@@ -0,0 +1,21 @@
+package models;
+
+public abstract class IngredienteDecorator extends Bevanda {
+
+    protected Bevanda bevanda;
+
+    public IngredienteDecorator(String descrizione, float costo, Bevanda bevanda) {
+        super(descrizione, costo);
+        this.bevanda = bevanda;
+    }
+
+    @Override
+    public String getDescrizione() {
+        return bevanda.getDescrizione() + " + " + super.getDescrizione();
+    }
+
+    @Override
+    public float getCosto() {
+        return bevanda.getCosto() + super.getCosto();
+    }
+}
