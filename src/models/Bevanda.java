684de27d8ef97aca81caa2e4789ceb95dfd3cac8
BilardoMauro02 <maurobilardo02@gmail.com>
2026-05-07 15:03:05 +0200

    Add: classe astratta bevanda

--- src/models/Bevanda.java
+++ src/models/Bevanda.java
@@ -1,4 +1,6 @@
 package models;
 
 public abstract class Bevanda {
+    public void getDescrizione(){System.out.println("DESCRIZIONE BASE");}
+    public void getCosto(){System.out.println("COSTO BEVANDA");}
 }
